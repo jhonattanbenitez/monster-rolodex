2fc02b69139af85b33c6492af3984af72348e497
Jhonattan <jhonattan.benitez@hotmail.com>
2020-07-29 19:38:55 -0500

    search box input added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      SearchField: "",
     };
   }
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}>
-        
-        </CardList>
-        </div> 
+        <input
+          type="search"
+          placeholder="Search monsters"
+          onChange={(e) => this.setState({ SearchField: e.target.value })}
+        />
+        <CardList monsters={this.state.monsters} />
+      </div>
     );
   }
- 
 }
 export default App;
-
-
-
-
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './card-list.style.css'
-import {Card} from '../card/card.component'
+import { Card } from '../card/card.component'
+
 export const CardList = props => {
     console.log(props)
     return <div className="card-list">
