b68c183b29580a4f46987937392c6226ddf64589
Jhonattan <jhonattan.benitez@hotmail.com>
2020-07-30 19:46:03 -0500

    Updates

---
+++ precache-manifest.80580a2bfb30d8340a239d9d100242fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af79b448f9a07a3eb0c2ee983a63e281",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "8ee98fec2fdda0c1ede1",
+    "url": "/monster-rolodex/static/css/main.5462ecbf.chunk.css"
+  },
+  {
+    "revision": "ab73865b93919c61e88f",
+    "url": "/monster-rolodex/static/js/2.1014887b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1014887b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee98fec2fdda0c1ede1",
+    "url": "/monster-rolodex/static/js/main.3e1e89f7.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
